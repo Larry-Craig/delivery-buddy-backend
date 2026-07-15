2ef4a2ea4379b62a01679316e40c107acaf3357f
Larry-Craig <chewachongcraig@gmail.com>
2026-07-16 00:50:56 +0100

    Update Swagger config to include v1 server path

--- src/main.ts
+++ src/main.ts
@@ -38,9 +38,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // Optional API Prefix
-  // app.setGlobalPrefix('api');
-
   // Swagger
   const config = new DocumentBuilder()
     .setTitle('Delivery Buddy API')
@@ -48,6 +45,7 @@ async function bootstrap() {
       'REST API powering the Delivery Buddy application.',
     )
     .setVersion('1.0.0')
+    .addServer('/v1') // This ensures Swagger builds the correct path for versioned endpoints
     .addTag('Drivers')
     .addTag('Orders')
     .addTag('Shifts')
